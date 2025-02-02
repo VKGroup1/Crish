680def382df9088d7c094edeb6ce4451742425c8
VKGroup1 <155010512+VKGroup1@users.noreply.github.com>
2025-02-02 09:04:50 +0530

    Add files via upload
    Signed-off-by: VKGroup1 <155010512+VKGroup1@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector('.container');
+    container.addEventListener("click", function() {
+        alert("Happy Rose Day! JihoonInc_ loves Chrish ❤️");
+    });
+});
